50d94832aabe122b1127de36b4b7741a1a615332
Adin H. Baber <AHBaber>
2026-09-21 17:20:10 -0500

    Pre-Release 0.6.6 Added support for dependent async library

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="StrictMock",
-    version="0.6.5",
+    version="0.6.6",
     description="Highly Deterministic Mocking",
     license="BSD-3-Clause",
     packages=find_packages(include=["strict_mock", "strict_mock.*"]),  # Specify the package folder
